a4ca63cc50b1c06073ab4bbf5c5284f2ee3289fc
erikaperciliano <erikaperciliano.developer@gmail.com>
2024-12-27 15:04:47 +0000

    creates Card Icon & Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,6 +5,8 @@ import './navigation.styles.scss'
 import { useContext } from "react"
 import { UserContext } from "../../components/contexts/user.context"
 import { signOutUser } from "../../utils/firebase/firebase.utils"
+import CartIcon from "../../components/cart-icon/cart-icon.component"
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
 
 const Navigation = () =>  {
     const { currentUser } = useContext(UserContext);
@@ -28,7 +30,9 @@ const Navigation = () =>  {
                             SIGN IN
                         </Link>
                     )}
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet/>
         </>
