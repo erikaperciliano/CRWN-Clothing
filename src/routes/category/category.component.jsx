d8148084f3cfaea857b11686496b709ad392ff6e
erikaperciliano <erikaperciliano.developer@gmail.com>
2025-01-07 16:49:15 +0000

    add asynchronous Redux-Thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,13 +3,15 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap} from '../..//store/categories/category.selector';
+import { selectCategoriesMap, selectIsLoading} from '../..//store/categories/category.selector';
 
+import Spinner from '../../components/spinner/spinner.component';
 import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -19,12 +21,18 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+       {
+          isLoading ? (
+            <Spinner /> 
+          ) : (
+                <CategoryContainer>
+                  {products &&
+                    products.map((product) => (
+                      <ProductCard key={product.id} product={product} />
+                    ))}
+                </CategoryContainer>
+              )
+       }
     </Fragment>
   );
 };
