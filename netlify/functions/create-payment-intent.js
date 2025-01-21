3af6c9d64db8c8b54b50d403e39db100a50cb1d5
erikaperciliano <erikaperciliano.developer@gmail.com>
2025-01-21 12:52:25 +0000

    adds the correct name to currency parameter

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
 
         const paymentIntent = await stripe.paymentIntents.create({
             amount,
-            current: "usd",
+            currency: "usd",
             payment_method_types: ["card"]
         });
 
